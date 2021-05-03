374cb7654bd34d7e989ccdfc690b3ea15c996944
Castillo <905903@cognizant.com>
2021-05-03 17:46:12 -0500

    Finish project with Mihir

--- src/test/java/org/example/shoppingcart/ShoppingCartTest.java
+++ src/test/java/org/example/shoppingcart/ShoppingCartTest.java
@@ -72,3 +72,4 @@ public class ShoppingCartTest {
         assertEquals(testShoppingCart.getQuantity(testItem1), 2);
     }
 }
+                                                                                                                                      
